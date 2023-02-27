3e8779c348bcc842e1cebe199be3e3a577ec2f9f
Uday <udaybharadwa2002@gmail.com>
2023-02-27 13:09:15 +0530

    Updated

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -10,8 +10,8 @@ const HomeContainer = () => {
       id="home"
     >
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">
+        <div className="flex items-center gap-2 justify-center bg-purple-100 px-4 py-1 rounded-full">
+          <p className="text-base text-purple-500 font-semibold">
             Bike Delivery
           </p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
@@ -25,7 +25,7 @@ const HomeContainer = () => {
 
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
           The Fastest Delivery in
-          <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
+          <span className="text-purple-600 text-[3rem] lg:text-[5rem]">
             Your City
           </span>
         </p>
@@ -39,7 +39,7 @@ const HomeContainer = () => {
 
         <button
           type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+          className="bg-gradient-to-br from-purple-400 to-purple-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Order Now
         </button>
--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -13,7 +13,7 @@ const MenuContainer = () => {
   return (
     <section className="w-full my-6" id="menu">
       <div className="w-full flex flex-col items-center justify-center">
-        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
+        <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-purple-400 to-purple-500 transition-all ease-in-out duration-100 mr-auto">
           Our Hot Dishes
         </p>
 
